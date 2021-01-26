e78d2e86182500385d8df0c2027a5959a06b5f12
Mario <mariopineda179@gmail.com>
2021-01-26 00:10:11 -0600

    set dark/light mode

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ const formInput = document.getElementById('todo-form')
 const checkInput = document.querySelectorAll('#todo')
 const deleteBtn = document.querySelector('.delete-completed')
 const state = document.querySelector('.state')
-const previousInputClass = localStorage['inputClass']
+const dragText = document.getElementById('drag-drop')
+const controls = document.querySelector('.controls')
+const themeSelector = document.getElementById('theme-selector')
 
-function main(){
-    document.addEventListener('DOMContentLoaded',getTodos )
-    itemsLeft()
-    form.addEventListener('submit', addTodo)
-    deleteBtn.addEventListener('click',deleted)
-    state.addEventListener('click',filterTodos)
-    todosHolder.addEventListener('click', addCompleted)
-}
 
+document.addEventListener('DOMContentLoaded', renderFirst )
+itemsLeft()
+form.addEventListener('submit', addTodo)
+deleteBtn.addEventListener('click',deleted)
+state.addEventListener('click',filterTodos)
+todosHolder.addEventListener('click', addCompleted)
+themeSelector.addEventListener('click', setTheme)
 
 function addTodo(e){
     e.preventDefault()
     /* create todo div */
     const todo = document.createElement('div');
     todo.classList.add('todo')  /* add a todo class to div */
-    todo.classList.add('active') 
+    todo.classList.add('active')
     /* create checkbox */
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox'
@@ -37,47 +38,65 @@ function addTodo(e){
     itemsLeft()
 }
 
+function renderFirst(){
+    getTodos()
+    previousTheme()
+}
 
 function addCompleted(e){
     const item = e.target
     if(item.id == 'todo'){
-        
-    // item.parentElement.classList.remove('active')
-    // item.classList.toggle('input-completed')
-    // item.parentElement.classList.toggle('completed')
-    index = document.querySelectorAll('#todo')
-        // console.log(item)
+        let index = document.querySelectorAll('#todo')
         for(let i = 0; i < index.length; i++){
+            
             if(item == index[i]){
-                console.log(index[i])
-                if(item.parentElement.classList.contains('active')){    
-                    // console.log(document.querySelectorAll('#todo')[i])
-                    if(index[i].classList.contains('input-completed')){
-                        console.log(index[i])
-                        index[i].parentElement.classList.remove('active')
-                        index[i].classList.toggle('input=completed')
-                        index[i].classList.toggle('completed')
-                        // localStorage['todoClass'] = ['completed']
-                    }
+                let todoValue = index[i].nextElementSibling.innerHTML
+                console.log(index[i].nextElementSibling.innerHTML)
+                if(!index[i].parentElement.classList.contains('completed')){
+                    console.log(index[i].parentElement)
+                    index[i].parentElement.classList.remove('active')
+                    index[i].classList.toggle('input-completed')
+                    index[i].parentElement.classList.toggle('completed')
+
+                    
+                    let existingTodos = localStorage.getItem('todos')
+                    
+                    existingTodos = existingTodos ? JSON.parse(existingTodos) : {};
+
+                    existingTodos.forEach(existingTodo =>{
+                        console.log(existingTodo)
+                        if(todoValue == existingTodo.value){
+                            existingTodo['completed'] = true
+                            itemsLeft()
+                            localStorage.setItem('todos', JSON.stringify(existingTodos))
+                        }
+                    })
                 }else{
                     item.parentElement.classList.add('active')
                     item.classList.toggle('input-completed')
                     item.parentElement.classList.toggle('completed')
-                }
+                    // localStorage.setItem('todos', JSON.stringify(existingTodos))
+                    let existingTodos = localStorage.getItem('todos')
+
+                    existingTodos = existingTodos ? JSON.parse(existingTodos) : {};
+                    existingTodos.forEach(existingTodo =>{
+                        console.log(existingTodo)
+                        if(todoValue == existingTodo.value){
+                            existingTodo['completed'] = false
+                            itemsLeft()
+                            localStorage.setItem('todos', JSON.stringify(existingTodos))
+                        }
+                    })
+                } 
+                // localStorage.setItem('todos', JSON.stringify(existingTodos))
             }
-            
-
                 }
-
-           
-
-        
         itemsLeft()
-    } 
+    }
 }
-           
-    
-    
+
+
+
 
 function deleted(){
     const todos = document.querySelectorAll('.todo')
@@ -86,6 +105,7 @@ function deleted(){
             deletedTodos(todo)
             todo.remove()
         }
+        itemsLeft()
     })
 }
 function itemsLeft(){
@@ -105,7 +125,7 @@ function filterTodos(e){
 
     for(let i = 0; i < state.length; i++ ){
         if(state[i] != currentLocation){
-            state[i].className = '' 
+            state[i].className = ''
         }else{
              state[i].className = 'controls-position'
         }
@@ -128,13 +148,13 @@ function filterTodos(e){
             }
             break;
         case 'all':
-            
+
             todo.style.display ='block'
             break;
-    } 
+    }
     })
-    
-    
+
+
 }
 
 function saveTodos(todo){
@@ -144,7 +164,11 @@ function saveTodos(todo){
     } else{
         todos = JSON.parse(localStorage.getItem('todos'))
     }
-    todos.push(todo);
+    let todoObj = {
+        value: todo,
+        completed: false
+    }
+    todos.push(todoObj);
     localStorage.setItem('todos', JSON.stringify(todos))
 }
 
@@ -155,11 +179,13 @@ function deletedTodos(todo){
         }else{
             todos = JSON.parse(localStorage.getItem('todos'));
         }
-
-        const todoIndex = todo.children[0].nextElementSibling.innerText
-        console.log(todoIndex)
-        todos.splice(todos.indexOf(todoIndex),1)
-        localStorage.setItem('todos',JSON.stringify(todos))
+        for(let i = 0; i < todos.length; i++){
+            if(todos[i]['completed'] === true){
+                todos.splice(i,1)
+                localStorage.setItem('todos', JSON.stringify(todos))
+            }
+        } 
+        itemsLeft()
 }
 
 function getTodos(){
@@ -167,44 +193,104 @@ function getTodos(){
     if(localStorage.getItem('todos') === null){
         todos = []
     }else{
-        todos = JSON.parse(localStorage.getItem('todos'));
+        todos = JSON.parse(localStorage.getItem('todos'))
+        console.log(todos)
     }
-    todos.forEach(function(newtodo){
-        let previousClass = localStorage['todoClass'];
-        
-        /* create todo div */
-        const todo = document.createElement('div');
-        todo.classList.add('todo')  /* add a todo class to div */
-        todo.classList.add('active')
+    todos.forEach(todo =>{
+        const div = document.createElement('div');
+        div.classList.add('todo');
+        // 
 
-        if(previousClass){
-            todo.classList.toggle(previousClass)
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.id = 'todo';
+
+        if(todo.completed == true){
+            div.classList.toggle('completed')
+            input.classList.toggle('input-completed')
+            console.log(todo)
+            
+        }else{
+            div.classList.add('active')
         }
+        div.appendChild(input)
 
-        /* create checkbox */
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox'
-        checkbox.id = 'todo'
-        todo.appendChild(checkbox)
-        /* create span */
         const span = document.createElement('span');
-        span.innerHTML = newtodo;
-        todo.appendChild(span)
-        todosHolder.appendChild(todo)
-        formInput.value = ''
+        span.innerHTML = todo.value;
+
+        div.appendChild(span)
+
+        todosHolder.appendChild(div)
         itemsLeft()
-    })
-    let todoCompleted
-    if(localStorage.getItem('todoCompleted') === null){
-        todoCompleted = []
+  })
+}
+
+function previousTheme(e){
+    const lightMode = document.getElementById('light-mode')
+    const darkMode = document.getElementById('dark-mode')
+    const todos = document.querySelectorAll('.todo')
+    const mainWrapper = document.querySelector('.main-wrapper')
+    let previousTheme = localStorage['theme']
+    console.log(previousTheme)
+    if (previousTheme == 'dark-mode'){
+        /* Dark Mode */
+        lightMode.style.display = 'none'
+        darkMode.style.display = 'flex'
+        /* Main Wrapper */
+        mainWrapper.classList.add('dark-mode')
+        mainWrapper.classList.remove('light-mode')
+        /* Input */
+        console.log(todosHolder)
+        formInput.classList.add('input-dark-mode')
+        /* Todos Holder */
+        todosHolder.classList.add('todos-dark-mode')
+        todosHolder.classList.remove('todos-light-mode')
+        /* Todo */
+        todos.forEach(todo =>{
+            todo.classList.add('todo-dark-mode')
+            todo.classList.remove('todo-light-mode')
+        })
+        
+        /* Controls */
+        controls.classList.add('controls-dark-mode')
+        controls.classList.remove('controls-light-mode')
+
+
     }else{
-        todoCompleted = JSON.parse(localStorage.getItem('todoCompleted'));
+        /* Light Mode */
+        lightMode.style.display = 'flex'
+        darkMode.style.display = 'none'
+        /* Main Wrapper */
+        mainWrapper.classList.remove('dark-mode')
+        mainWrapper.classList.add('light-mode')
+        /* Input */
+        formInput.classList.remove('input-dark-mode')
+        /* Todos Holder */
+        todosHolder.classList.add('todos-light-mode')
+        todosHolder.classList.remove('todos-dark-mode')
+        /* Todo */
+        todos.forEach(todo =>{
+            todo.classList.remove('todo-dark-mode')
+            todo.classList.add('todo-light-mode')
+        })
+        /* Controls */
+        controls.classList.add('controls-light-mode')
+        controls.classList.remove('controls-dark-mode')
+ 
     }
-    todoCompleted.forEach(function(completed){
-        if(completed in todos ){
-            //
-        }
-    })
     
 }
-main()
+
+function setTheme(e){
+    let theme = e.target.id
+    console.log('event working')
+    if(theme == 'light-mode'){
+        /* Save Theme */
+        localStorage['theme'] = 'dark-mode'
+        previousTheme()
+    }else{
+        /* Save Theme */
+        localStorage['theme'] = 'light-mode'
+        previousTheme()
+    }
+}
